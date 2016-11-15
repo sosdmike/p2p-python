67a7721ad2abbd227eb78f239232ea463d87551f
Perez, James <James.Perez3@latimes.com>
2016-11-14 18:25:04 -0800

    Moved to 2.0.24

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p-latimes",
-    version="2.0.23",
+    version="2.0.24",
     packages=find_packages(),
     install_requires=(
         "python-dateutil",
