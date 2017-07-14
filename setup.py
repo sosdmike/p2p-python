eadde79c2aacc9174085c117a97b10db85aeb487
palewire <ben.welsh@gmail.com>
2017-07-13 17:35:02 -0700

    v2.0.34

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p-latimes",
-    version="2.0.33",
+    version="2.0.34",
     packages=find_packages(),
     install_requires=(
         "python-dateutil",
